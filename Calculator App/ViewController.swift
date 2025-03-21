e512424e7543d71b300bf07f0590b9c4932982f0
jscheth <sovichea.chet@gmail.com>
2025-03-20 21:51:53 -0700

    Initial Commit

---
+++ Calculator App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator App
+//
+//  Created by Jonathan Cheth on 3/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Calculator AppTests/Calculator_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_AppTests.swift
+//  Calculator AppTests
+//
+//  Created by Jonathan Cheth on 3/20/25.
+//
+
+import Testing
+@testable import Calculator_App
+
+struct Calculator_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
